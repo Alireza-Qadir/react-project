69335395116868cf9accefec0c05bb4652582524
AlirezaQ <Aliq.90.h@gmail.com>
2022-03-28 00:01:50 +0430

    navbar responsive complete

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,11 @@
 import React , {useState} from "react";
 import {Link , BrowserRouter as Router} from "react-router-dom";
-import 'remixicon/fonts/remixicon.css'
+import { Divide as Hamburger } from 'hamburger-react'
+
+import 'remixicon/fonts/remixicon.css';
+import 'animate.css';
+
+
 import {ReactComponent as LogoSvg} from './diamond.svg';
 import "./navbar.scss"
 import 'hover.css';
@@ -10,19 +15,46 @@ import 'hover.css';
    
         const Navbar = () =>
         {
+
+       
           const [logoHover , setlogoHover] = useState(false)
+          const [isOpen, setOpen] = useState(false)
+
      
        return(
      
      <Router>
-         <nav className="w-full h-auto flex lg:flex-row  md:flex-col sm:flex-col flex-col md:flex-col justify-evenly  items-center" id="navbar">
-       
-           <div className="lg:w-2/12 md:w-full sm:w-full flex justify-center">
-           <Link>  
-              <img src='./images/logo.jpg' alt='logo' className='w-3/4 cursor-pointer' style={{     width:"200px"}}/>
-           </Link>
+       <div className="lg:hidden md:flex sm:flex flex justify-between "> 
+             <Hamburger toggled={isOpen} toggle={setOpen} />
+
+             {
+                 !isOpen 
+                 ?
+                 <div className="lg:w-2/12 md:w-2/12 w-2/12 flex">
+              <Link to="/#">  
+                 <img src='./images/logo.jpg' alt='logo' className='w-3/4 cursor-pointer' style={{ width:"200px"}}/>
+              </Link>
          
+            </div>
+            :
+            null
+             }
+
+            
          </div>
+
+         
+         
+
+         <nav className={`w-full h-auto flex lg:flex-row  md:flex-col sm:flex-col flex-col md:flex-col justify-evenly  items-center ${ isOpen ? "animate__animated animate__lightSpeedInLeft " : null }`} id="navbar">
+       
+         <div className="lg:w-2/12 md:w-full sm:w-full flex justify-center">
+              <Link to="/#">  
+                 <img src='./images/logo.jpg' alt='logo' className='w-3/4 cursor-pointer' style={{ width:"200px"}}/>
+              </Link>
+         
+            </div>
+         
      
      <ul className="lg:w-6/12  flex lg:flex-row md:flex-col sm:flex-col flex-col justify-evenly text-gray-600 ">
          <li className="cursor-pointer py-1 px-0.5 hvr-underline-from-center">
